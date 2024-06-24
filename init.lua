fae64f201865e14df318b4a77c0549572a1214a6
arctenis <arctenischloria@gmail.com>
2024-06-24 16:24:07 +0200

    Set 'fd' keybinding to cancel highlight

--- init.lua
+++ init.lua
@@ -162,7 +162,7 @@ vim.opt.scrolloff = 10
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', 'fd', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
