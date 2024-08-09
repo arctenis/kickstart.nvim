2fd7fc12ac050f74e5e200472c0c4677ae469163
arctenis <arctenischloria@gmail.com>
2024-08-09 06:16:00 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -624,7 +624,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'prettier',
+        'prettier', -- Used to format HTML, CSS, etc.
+        'ruff', -- Used to format Python code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -670,7 +671,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff' },
+        python = { 'ruff', 'black' },
         html = { 'prettier' },
         css = { 'prettier' },
         htmldjango = { 'djlint' },
